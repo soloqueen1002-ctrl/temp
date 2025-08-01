68a1c2f0a2285f88719f197d6f2fbc36c2923272
raakesh-m <getbandark@gmail.com>
2025-08-01 14:11:18 +0530

    Update API URL paths to remove 'api/' prefix for user signup, login, hotel booking, food ordering, and order retrieval endpoints. Update database file.

--- django/backend/myapi/urls.py
+++ django/backend/myapi/urls.py
@@ -8,9 +8,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path('api/signup/', signup_view, name='signup'),                # User signup
-    path('api/login/', login_view, name='login'),                  # User login
-    path('api/book-hotel/', hotel_booking_view, name='book_hotel'),# Hotel booking
-    path('api/order-food/', food_order_view, name='order_food'),   # Food ordering
-    path('api/my-orders/', get_user_orders, name='my_orders'),     # Get orders by user
+    path('signup/', signup_view, name='signup'),                # User signup
+    path('login/', login_view, name='login'),                  # User login
+    path('book-hotel/', hotel_booking_view, name='book_hotel'),# Hotel booking
+    path('order-food/', food_order_view, name='order_food'),   # Food ordering
+    path('my-orders/', get_user_orders, name='my_orders'),     # Get orders by user
 ]
